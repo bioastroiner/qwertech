d102a2d48f384c3bfce040c26abee1652783f4a3
bioastroiner <naderi81eco@gmail.com>
2023-03-04 12:47:09 +0330

    Update Buildscript

--- src/main/java/com/kbi/qwertech/QwerTech.java
+++ src/main/java/com/kbi/qwertech/QwerTech.java
@@ -93,7 +93,7 @@ import static gregapi.data.TD.Properties.HAS_TOOL_STATS;
 public final class QwerTech extends Abstract_Mod {
     public static final String MODID = "qwertech";
     public static final String MODNAME = "QwerTech";
-    public static final String VERSION = "${version}";
+    public static final String VERSION = "1.0.0-a.50";
     public static ModData MOD_DATA = new ModData(MODID, MODNAME);
     public static QwerTech instance;
     
